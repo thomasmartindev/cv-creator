a9f00379ac60e1230663273cc0b7518ecff53317
Thomas Martin <thomasmdevelopment@gmail.com>
2023-01-27 22:19:05 +1000

    Add colours

--- src/theme/Theme.js
+++ src/theme/Theme.js
@@ -1,7 +1,10 @@
 export const Theme = {
   background: '#D7D7D7', // 215, 215, 215
   dark: '#2D2D2D', // 45, 45, 45
-  light: '#EBEBEB' // 235, 235, 235
+  green: '#317873',
+  light: '#F5F5F5', // 245, 245, 245
+  mid: '#CDCDCD', // 205, 205, 205
+  midLight: '#EBEBEB' // 235, 235, 235
 }
 
 export default Theme
