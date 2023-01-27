f4fff297ac43b7e386f42e3ae0d11b37c90ce056
Thomas Martin <thomasmdevelopment@gmail.com>
2023-01-27 22:18:43 +1000

    Add styles

--- src/theme/GlobalStyle.js
+++ src/theme/GlobalStyle.js
@@ -4,18 +4,25 @@ export const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
     margin: 0;
+    overflow-wrap: break-word;
     padding: 0;
   }
 
   html {
-    font-family: 'Roboto', sans-serif;
     font-size: 62.5%;
   }
 
   body {
     background-color: ${({ theme }) => theme.background};
+    font-family: 'Roboto', sans-serif;
     font-size: 1.6rem;
   }
 
-  h1 { font-size: 100%; }
+  h1 {
+    font-size: 100%;
+  }
+
+  button {
+    cursor: pointer;
+  }
 `
